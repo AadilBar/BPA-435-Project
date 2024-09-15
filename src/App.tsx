a48c2134af96351ef79746302cda1303c64782c6
Aadil Barkat <aadilbar2008@gmail.com>
2024-09-14 20:35:54 -0500

    fixed issue with app.tsx which caused buidl to fail

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 // import * as React from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
 import Navbar from './components/navbar.tsx';
-import { BrowserRouter as Router, Route, Routes, BrowserRouter } from 'react-router-dom';
+import {Route, Routes, BrowserRouter } from 'react-router-dom';
 import Home from './pages/Home.tsx';
 import About from './pages/About.tsx';
 import Store from './pages/Store.tsx';
@@ -17,7 +17,6 @@ function App(){
                     <Route path="/about-us" element={<About />} />
                     <Route path="/tour" element={<Tour />} />
                     <Route path="/store" element={<Store />} />
-
                 </Routes>
             </BrowserRouter>
         </ChakraProvider>
